98dcdde8c53fe9c2796984035a4295b970451ae3
Jason R. Coombs <jaraco@jaraco.com>
2012-09-01 09:07:49 -0400

    Fleshed out writer a bit

--- thwump/writing.py
+++ thwump/writing.py
@@ -1,4 +1,14 @@
 import whoosh.writing
 
 class IndexWriter(whoosh.writing.IndexWriter):
-	"stubbed"
+	def __init__(self, index):
+		self.index = index
+
+	def add_document(self, **fields):
+		self.index.collection.insert(fields)
+
+	def add_reader(self):
+		raise NotImplementedError("stubbed")
+
+	def delete_document(self, docnum, delete=True):
+		self.index.collection.remove(docnum)
