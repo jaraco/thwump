2a18de0fe93e1e7208f119d8730146e85bd062c9
Jason R. Coombs <jaraco@jaraco.com>
2012-10-05 10:38:20 -0400

    Fleshed out Writer.add_reader with my guess at what it might be supposed to be doing.

--- thwump/writing.py
+++ thwump/writing.py
@@ -7,8 +7,14 @@ class IndexWriter(whoosh.writing.IndexWriter):
 	def add_document(self, **fields):
 		self.index.collection.insert(fields)
 
-	def add_reader(self):
-		raise NotImplementedError("stubbed")
+	def add_reader(self, reader):
+		"""
+		Add the documents from the reader to the writer (?). Parent class
+		doesn't document what this is supposed to do.
+		Return None
+		"""
+		for item in reader.index.collection.find():
+			self.index.insert(item)
 
 	def delete_document(self, docnum, delete=True):
 		self.index.collection.remove(docnum)
