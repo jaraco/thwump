ad293144e9d6ca50c6eaa5142adf7e39e66bf78f
Jason R. Coombs <jaraco@jaraco.com>
2012-10-05 10:53:48 -0400

    Allow a schema to be passed

--- thwump/index.py
+++ thwump/index.py
@@ -7,9 +7,11 @@ from . import writing
 class Index(whoosh.index.Index):
 	database_name = 'thwump'
 	collection_name = 'index'
-	def __init__(self, *args, **kwargs):
-		self.connection = pymongo.Connection(*args, **kwargs)
-		self.collection = self.connection[self.database_name][self.collection_name]
+	def __init__(self, schema=None, connection_factory=pymongo.Connection):
+		self.schema = schema
+		self.connection = connection_factory()
+		self.collection = self.connection[self.database_name][
+			self.collection_name]
 
 	def is_empty(self):
 		return not self.doc_count()
